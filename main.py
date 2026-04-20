b2a5fcd0a07e7ce0050c6367a9debf03493f7281
AleDav73 <161150672+AleDav73@users.noreply.github.com>
2026-04-20 01:26:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import platform
 import sys
 # Archivos del proyecto
 from interface import Interface
+from capture import Capture
 
 # Comprobar si existe en el dispositivo "npcap" (preferentemente) no "wincap".
 # por cuestiones de funcionamiento de "scapy".
@@ -17,3 +18,22 @@ if __name__ == '__main__':
     print(f'Wi-Fi {interfaceWifi}\n', f'Ethernet {interfaceEthernet}\n', f'Loopback {interfaceLoopback}\n')
     
     # Ejecución preliminar del programa.
+
+  
+    # AGREGADO PARA LA FASE DE CAPTURA
+    interfaz_elegida = None
+    if interfaceWifi:
+        interfaz_elegida = interfaceWifi[0]
+    elif interfaceEthernet:
+        interfaz_elegida = interfaceEthernet[0]
+    elif interfaceLoopback:
+        interfaz_elegida = interfaceLoopback[0]
+        
+    if interfaz_elegida:
+        # Se llama al módulo de captura
+        datos_crudos = Capture.iniciar_captura(interfaz_elegida)
+        
+        if datos_crudos:
+            print("\n[*] Captura estática completada. Datos listos para la fase de análisis.")
+    else:
+        print("\n[!] No se encontró ninguna interfaz activa para iniciar la captura.")
